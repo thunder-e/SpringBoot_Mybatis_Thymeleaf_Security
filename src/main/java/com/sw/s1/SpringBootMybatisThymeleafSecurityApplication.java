125ab3b6581c4056bf09a086996e506c59d3b00d
thunder-e <pinkpear0114@naver.com>
2021-05-24 09:42:16 +0900

    Initial commit

---
+++ src/main/java/com/sw/s1/SpringBootMybatisThymeleafSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sw.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisThymeleafSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisThymeleafSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sw/s1/SpringBootMybatisThymeleafSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisThymeleafSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
